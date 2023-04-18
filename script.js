0c4d19f674a8f76a075e9c439420c371e6e6a9d5
leehatban <130417446+leehatban@users.noreply.github.com>
2023-04-18 11:48:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,133 @@
-const display = document.querySelector('.display');
-const buttons = document.querySelectorAll('.buttons button');
+const display = document.querySelector('.calculator-display');
 
-let result = '';
+const numberButtons = document.querySelectorAll('[data-number]');
 
-buttons.forEach(button => {
-  button.addEventListener('click', function() {
-    const value = this.textContent;
-    if (value === 'C') {
-      result = '';
-      display.textContent = '';
-    } else if (value === '=') {
-      display.textContent = eval(result);
-      result = '';
-    } else {
-      result += value;
-      display.textContent = result;
-    }
-  });
+const operatorButtons = document.querySelectorAll('[data-operator]');
+
+const clearButton = document.querySelector('[data-clear]');
+
+const equalsButton = document.querySelector('[data-equals]');
+
+ 
+
+let currentValue = '';
+
+let previousValue = '';
+
+let currentOperator = null;
+
+let shouldResetDisplay = false;
+
+ 
+
+numberButtons.forEach(button => {
+
+button.addEventListener('click', () => {
+
+if (shouldResetDisplay) {
+
+display.value = '';
+
+shouldResetDisplay = false;
+
+}
+
+display.value += button.textContent;
+
 });
+
+});
+
+ 
+
+operatorButtons.forEach(button => {
+
+button.addEventListener('click', () => {
+
+if (currentOperator) calculate();
+
+previousValue = display.value;
+
+currentOperator = button.dataset.operatorType;
+
+shouldResetDisplay = true;
+
+});
+
+});
+
+ 
+
+equalsButton.addEventListener('click', () => {
+
+if (!currentOperator) return;
+
+calculate();
+
+});
+
+ 
+
+clearButton.addEventListener('click', () => {
+
+currentValue = '';
+
+previousValue = '';
+
+currentOperator = null;
+
+display.value = '';
+
+});
+
+ 
+
+function calculate() {
+
+currentValue = display.value;
+
+let result;
+
+ 
+
+switch (currentOperator) {
+
+case 'add':
+
+result = parseFloat(previousValue) + parseFloat(currentValue);
+
+break;
+
+case 'subtract':
+
+result = parseFloat(previousValue) - parseFloat(currentValue);
+
+break;
+
+case 'multiply':
+
+result = parseFloat(previousValue) * parseFloat(currentValue);
+
+break;
+
+case 'divide':
+
+result = parseFloat(previousValue) / parseFloat(currentValue);
+
+break;
+
+default:
+
+return;
+
+}
+
+ 
+
+display.value = result;
+
+currentOperator = null;
+
+shouldResetDisplay = true;
+
+}
